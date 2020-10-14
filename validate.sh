58dec64e789ace02143e776f904db9c6a4c62e71
Marcel Susky <marcel.susky@tu-dresden.de>
2020-10-14 15:28:04 +0200

    Update validate.sh to download validator CLI if not present

--- validate.sh
+++ validate.sh
@@ -1,16 +1,17 @@
 #/usr/bin/env sh
 
-if [ -f ./validator_cli.jar ]; then
-    java -jar ./validator_cli.jar Examples \
-         -ig DerivedProfiles \
-         -ig basisprofil-de-r4/extensions \
-         -ig basisprofil-de-r4/datentypen-profile \
-         -ig basisprofil-de-r4/ressourcen-profile \
-         -ig basisprofil-de-r4/terminologie \
-         -profile http://helict.de/fhir/StructureDefinition/BasisPatient \
-         -version 4.0.1 \
-	 -recurse -debug
-else
-    echo "Please download the validator and run again: https://github.com/hapifhir/org.hl7.fhir.core/releases/latest/download/validator_cli.jar"
-fi
+# Check if java runtime is installed
+[ -z "$(command -v java)" ] && echo "Please install java runtime environment: https://openjdk.java.net/install" && exit 1
 
+# Download FHIR validator if not exist and validate FHIR resources
+[ ! -f ./validator_cli.jar ] && wget -q https://github.com/hapifhir/org.hl7.fhir.core/releases/latest/download/validator_cli.jar
+
+java -jar ./validator_cli.jar Examples \
+     -ig DerivedProfiles \
+     -ig basisprofil-de-r4/extensions \
+     -ig basisprofil-de-r4/datentypen-profile \
+     -ig basisprofil-de-r4/ressourcen-profile \
+     -ig basisprofil-de-r4/terminologie \
+     -profile http://helict.de/fhir/StructureDefinition/BasisPatient \
+     -version 4.0.1 \
+     -recurse -debug
